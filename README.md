f43065664b7d8f733b061204513b57b655136a2c
Yuheng Kuang <kuangyuheng@gmail.com>
2016-04-05 15:54:47 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # pyconf
+
 A Python DSL for generating JSON or Protobuf configuration
+
+
+## Basic Idea
+
+- Confiugration is pure data, that can be easily serialized to JSON or Protobuf.
+- Pyconf is a Python-based DSL, which is also a valid Python program, running
+  it results in configuration in pure data form.
+- `Node` holds configuration data, node is immutable.
+- If node holds dict data, it can be "extended" passing in passing key-value
+  pairs, extension results in new node with copy of original data updated with
+  key-value pairs.
+
+## Syntax
+
+Quite similar to [Bazel](https://bazel.io):
+
+    # Basic configuration
+    user = conf(
+      name = 'yuheng',
+      etc = conf(
+        lang = 'en',
+        editor = 'vi',
+      ),
+    )
+    user.execute() # {'name': 'yuheng', 'etc': {'lang': 'en', 'editor': 'vi'}}
+
+    # Invoking a node to extend it, note extension is shallow.
+    ann = user(
+      name = 'ann',
+      home = '/local/home/ann',
+      # user.etc returns a node containg data of 'etc' subfield
+      etc = user.etc(
+        editor = 'emacs',
+      ),
+    )
+    ann.execute() # {'name': 'ann', 'home': '/local/home/ann' 'etc': {'lang': 'en', 'editor': 'emacs'}}
+
+    # Mixin: extend by patching another node
+    use_emacs = conf(editor = 'emacs')
+    ann = user(
+      name = 'ann',
+      etc = (user.etc << use_emacs)(
+        lang = 'fr',
+      ),
+    )
+
+    # Appends to repeated field
+    ls = conf(cmd = 'ls')
+    ls_detailed = ls(flags =  conf.flags + ['-l'])
+
+    # Ordered tuple is also useful, you can individually extend or replace node
+    # in an ordered list
+    layout = box(
+      type = 'hflow',
+      children = conf['title body action_button'](
+        title = 'Hello',
+      ),
+    )
+    my = layout(children = layout.children(body = 'World'))
+    my.execute() # {'type': 'hflow', children: ['Hello', 'World', None]}
+
+    # Load annother conf
+    common = load('path/to/conf/common.pyconf')
+
+# Extension
+
+All valid Python code can be valid pyconf code (you can configure restricted
+environment if you want). It quite easy to define something like:
+
+    def my_complex_rule(name=None, deps=[], **kwargs):
+      # Returns a Node
+
+then use it like extending a normal node.
+
+You can also define a post converter that converts node data when outputing,
+ordered tupple introduced above uses this technique:
+
+    def order_handler(order):
+      return lambda meta, data: [data.get(k) for k in order]
+
+    # conf['title body'] is a shortcut of
+    conf('__post__': order_handler(['title', 'body']))
+
+# API
+
+See code
---
+++ conf.py
@@ -0,0 +1,119 @@
+"""
+Pyconf DSL for generating JSON or Protobuf configuration.
+"""
+
+class Node(object):
+  def __init__(self, value):
+    self._value = value
+
+  def execute(self):
+    def _unwrap(item):
+      if isinstance(item, Node):
+        return item.execute()
+      else:
+        return item
+
+    if isinstance(self._value, dict):
+      meta = {}
+      data = {}
+      for k, v in self._value.iteritems():
+        if k.startswith('__') and k.endswith('__'):
+          meta[k] = v
+        else:
+          data[k] = _unwrap(v)
+      if '__post__' in meta:
+        return meta['__post__'](meta, data)
+      else:
+        return data
+    elif isinstance(self._value, (list, tuple)):
+      return map(_unwrap, self._value)
+    else:
+      return self._value
+
+  def __call__(self, **kwargs):
+    output = {}
+    if self._value is not None:
+      if not isinstance(self._value, dict):
+        raise TypeError('Cannot extend non-dict node %s' % (type(self._value),))
+      output.update(self._value)
+    for k, v in kwargs.iteritems():
+      output[k] = v
+    return Node(output)
+
+  def __getattr__(self, attr):
+    if attr.startswith('_'):
+      raise AttributeError('Private or meta attr %s' % (attr,))
+    if self._value is None:
+      return self
+    if not isinstance(self._value, dict):
+      raise TypeError('Cannot get attr of non-dict node %s' % (type(self._value),))
+    output = self._value.get(attr)
+    if not isinstance(output, Node):
+      output = Node(output)
+    return output
+
+  def __getitem__(self, fn):
+    if callable(fn):
+      return self(__post__ = fn)
+    order = fn
+    if isinstance(order, (str, unicode)):
+      order = order.strip().split()
+    return self(__post__ = lambda meta, value: [value.get(key) for key in order])
+
+  def __add__(self, ls):
+    if isinstance(ls, Node):
+      ls = ls._value
+    if not isinstance(ls, (list, tuple)):
+      raise TypeError('Cannot append node with non-array data %s' % (type(ls),))
+    orig = self._value
+    if orig is None:
+      orig = ()
+    if not isinstance(orig, (list, tuple)):
+      raise TypeError('Cannot append non-array node %s' % (type(self._value),))
+    return Node(tuple(orig) + tuple(ls))
+
+  def __lshift__(self, right):
+    if isinstance(right, Node):
+      right = right._value
+    if not isinstance(right, dict):
+      raise TypeError('Cannot extend node with non-dict data %s' % (type(right),))
+    return self(**right)
+
+conf = Node(None)
+
+def array(*args):
+  return Node(args)
+
+def run(path, builtins=None):
+  import imp
+  import runpy
+  conf_builtins = {
+    'conf': conf,
+    'array': array,
+  }
+  if builtins is not None:
+    conf_builtins.update(builtins)
+
+  def conf_load(path):
+    result_dict = runpy.run_path(path, init_globals=conf_builtins)
+    mod = imp.new_module(path)
+    for k, v in result_dict.iteritems():
+      if k.startswith('_'):
+        continue
+      setattr(mod, k, v)
+    return mod
+  conf_builtins['load'] = conf_load
+
+  result = runpy.run_path(path, init_globals=conf_builtins)
+  output = {}
+  for k, v in result.iteritems():
+    if isinstance(v, Node):
+      output[k] = v.execute()
+  return output
+
+if __name__ == '__main__':
+  import json
+  import sys
+  ret = run(sys.argv[1])['CONFIG']
+  print json.dumps(ret)
+
